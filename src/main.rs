b639ee54f85f862a811e4ec6062c9347f2f008b8
Leandro Pacheco <contact@leandropacheco.com>
2016-03-04 21:52:25 +0100

    changes to main

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,33 @@ extern crate rand;
 
 use btree::BTree;
 use std::collections::BTreeMap;
+use std::io;
+use std::io::Read;
+
+const N:i32 = 10000000;
 
 fn main() {
     let mut t: BTree<i32, i32> = BTree::new(10); //
-    for n in 1..1000000 {
+    for n in 1..N {
         t.insert(n, n*2);
     }
 
     // let mut t: BTreeMap<i32, i32> = BTreeMap::new(); //
-    // for n in 1..1000000 {
+    // for n in 1..N {
     //     t.insert(n, n*2);
     // }
 
-    for n in 1..1000000 {
-        t.get(&n);
-    }
+    println!("press enter...");
+    io::stdin().read_line(&mut String::new());
 
-    let mut items = t.iter();
-    for n in 1..1000000 {
-        items.next();
-    }
+    // for n in 1..N {
+    //     t.get(&n);
+    // }
+
+    // let mut items = t.iter();
+    // for n in 1..N {
+    //     items.next();
+    // }
 
     // t.breath_first_print();
 
