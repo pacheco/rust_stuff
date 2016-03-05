c48c9a06c530afec9f0832fe4db79ca939bcaebf
Leandro Pacheco <contact@leandropacheco.com>
2016-03-05 10:47:45 +0100

    changes to btree main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-extern crate btree;
+extern crate rust_stuff;
 extern crate rand;
 
-use btree::BTree;
+use rust_stuff::BTree;
 use std::collections::BTreeMap;
 use std::io;
 use std::io::Read;
@@ -9,39 +9,46 @@ use std::io::Read;
 const N:i32 = 10000000;
 
 fn main() {
-    // let mut t: BTree<i32, i32> = BTree::new(10);
-    // for n in (1..N).rev() {
-    //     t.insert(n, n*2);
-    // }
-
-    // println!("height: {}", t.height());
-    // println!("len: {}", t.len());
-
-    let mut t: BTreeMap<i32, i32> = BTreeMap::new(); //
+    let mut t: BTree<i32, i32> = BTree::new(10);
     for n in (1..N).rev() {
         t.insert(n, n*2);
     }
 
+    // println!("height: {}", t.height());
+    // println!("len: {}", t.len());
+
+    // let mut t: BTreeMap<i32, i32> = BTreeMap::new();
+    // for n in (1..N).rev() {
+    //     t.insert(n, n*2);
+    // }
+
     // println!("press enter...");
     // io::stdin().read_line(&mut String::new());
 
-    // for n in 1..N {
-    //     t.get(&n);
+    for n in 1..N {
+        t.get(&n);
+    }
+
+    // {
+    //     let mut items = t.iter();
+    //     for n in 1..N {
+    //         items.next();
+    //     }
     // }
 
-    // let mut items = t.iter();
+    // let mut items = t.into_iter();
     // for n in 1..N {
     //     items.next();
     // }
 
     //t.breath_first_debug_print(false);
 
-    for n in 1..N {
-        match t.remove(&n) {
-            Some(i) => assert_eq!(i, n*2),
-            _ => panic!(n),
-        }
-    }
+    // for n in 1..N {
+    //     match t.remove(&n) {
+    //         Some(i) => assert_eq!(i, n*2),
+    //         _ => panic!(n),
+    //     }
+    // }
 
     // for kv in t.into_iter() {
     //     println!("{:?}", kv);
