8daa58a48d43c0a2dbb99e2d06189d752a2800a6
Leandro Pacheco <contact@leandropacheco.com>
2016-04-01 01:28:27 +0200

    basic async msg server working

--- examples/async.rs
+++ examples/async.rs
@@ -4,16 +4,22 @@ extern crate bytes;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate rand;
+extern crate slab;
 
+use rust_stuff::net::network_to_u32;
+use std::slice;
+use std::collections::{VecDeque, HashSet};
 use std::net::SocketAddr;
 use mio::*;
 use mio::tcp::*;
-use mio::util::*;
+use mio::util::Slab;
 use std::io;
-use bytes::{ByteBuf};
+use bytes::{ByteBuf, RingBuf, Buf};
 
 const MAX_CONNECTIONS: usize = 128;
 const MAX_MSG_SIZE: usize = 32*1024;
+const MSG_HDR_SIZE: usize = 4;
 
 const SERVER: Token = Token(0);
 
@@ -32,41 +38,44 @@ struct Server<H: ServerHandler> {
     token: Token,
     socket: TcpListener,
     connections: Slab<Connection>,
-    connections_new: Vec<TcpStream>,
-    connections_closed: Vec<Token>,
-    handler: H,
+    connections_new: VecDeque<TcpStream>,
+    connections_closed: Option<HashSet<Token>>,
+    connections_reregister: Option<HashSet<Token>>,
+    handler: Option<H>,
     shutdown: bool,
 }
 
 impl<H: ServerHandler> Server<H> {
-    /// Create and bind a to the given address
+    /// Create a new Server bound to the given address
     pub fn bind(addr: &SocketAddr, handler: H) -> Result<Self, Error> {
         let s = try!(TcpListener::bind(&addr));
         Ok(Server {
             token: Token(0),
             socket: s,
             connections: Slab::new_starting_at(Token(1), MAX_CONNECTIONS), // max number of concurrent connections?
-            connections_new: vec!(),
-            connections_closed: vec!(),
-            handler: handler,
+            connections_new: VecDeque::new(),
+            connections_closed: Some(HashSet::new()),
+            connections_reregister: Some(HashSet::new()),
+            handler: Some(handler),
             shutdown: false,
         })
     }
 
-    /// Register the server in the event loop
-    pub fn register(&self, evloop: &mut EventLoop<Self>) -> Result<(), Error> {
-        try!(evloop.register(&self.socket, self.token,
+    pub fn run(&mut self) -> Result<(), Error> {
+        let mut evl = try!(EventLoop::new());
+        try!(evl.register(&self.socket, self.token,
                              EventSet::readable(),
                              PollOpt::edge()));
+        try!(evl.run(self));
         Ok(())
     }
 
     fn accept(&mut self) {
         match self.socket.accept() {
-            Ok(Some((s, addr))) => {
-                self.connections_new.push(s);
+            Ok(Some((s, _))) => {
+                self.connections_new.push_back(s);
             }
-            Ok(None) => debug!("accept would block"),
+            Ok(None) => trace!("accept would block"),
             Err(err) => {
                 error!("error accepting connection: {}", err);
                 self.shutdown();
@@ -74,20 +83,18 @@ impl<H: ServerHandler> Server<H> {
         }
     }
 
-    fn shutdown(&mut self) {
-        info!("shutting down!");
-        self.shutdown = true;
-    }
-
     fn register_new_connections(&mut self, evloop: &mut EventLoop<Self>) {
-        while let Some(conn) = self.connections_new.pop() {
-            match self.connections.insert_with(|token| Connection::new(token, conn)) {
+        while let Some(conn) = self.connections_new.pop_front() {
+            match self.connections.insert_with(move |token| Connection::new(token, conn)) {
                 Some(token) => {
                     if self.connections[token].register(evloop).is_err() {
-                        error!("could not register new connection on event loop");
-                        self.shutdown();
-                        break;
+                        panic!("could not register new connection on event loop");
                     }
+                    let mut h = self.handler.take().unwrap();
+                    let uid = self.connections[token].uid.clone();
+                    trace!("new connection {:?}", uid);
+                    h.connection(self, uid);
+                    self.handler = Some(h);
                 }
                 None => {
                     error!("cannot accept new connection: limit reached");
@@ -96,17 +103,30 @@ impl<H: ServerHandler> Server<H> {
         }
     }
 
-    fn remove_closed_connections(&mut self, evloop: &mut EventLoop<Self>) {
-        while let Some(token) = self.connections_closed.pop() {
-            match self.connections.remove(token) {
-                Some(ref mut c) => {
-                    if c.deregister(evloop).is_err() {
-                        error!("error deregistering connection");
-                    }
-                }
-                None => panic!("connection not present on slab"),
+    fn reregister_connections(&mut self, evloop: &mut EventLoop<Self>) {
+        let mut uids = self.connections_reregister.take().unwrap();
+        for uid in uids.drain() {
+            if let Err(e) = self.connections[uid].reregister(evloop) {
+                panic!("error reregistering connection on event loop: {:?}", e);
             }
         }
+        self.connections_reregister = Some(uids);
+    }
+
+    fn remove_closed_connections(&mut self, evloop: &mut EventLoop<Self>) {
+        let mut to_close = self.connections_closed.take().unwrap();
+        for token in to_close.drain() {
+            trace!("removing connection {:?} from slab", token);
+            if let Some(ref mut c) = self.connections.remove(token) {
+                if c.deregister(evloop).is_err() {
+                    error!("error deregistering connection");
+                }
+                let mut h = self.handler.take().unwrap();
+                h.connection_closed(self, &c.uid);
+                self.handler = Some(h);
+            }
+        }
+        self.connections_closed = Some(to_close);
     }
 }
 
@@ -124,7 +144,7 @@ impl<H: ServerHandler> Handler for Server<H> {
     type Message = ();
     type Timeout = ();
 
-    fn ready(&mut self, evloop: &mut EventLoop<Self>, token: Token, events: EventSet) {
+    fn ready(&mut self, _evloop: &mut EventLoop<Self>, token: Token, events: EventSet) {
         match token {
             SERVER => {
                 if events.is_readable() {
@@ -135,103 +155,296 @@ impl<H: ServerHandler> Handler for Server<H> {
                 }
             }
             client => {
-                self.connections[client].ready(evloop, events);
+                let uid = self.connections[client].uid.clone();
+                // readable
+                if events.is_readable() {
+                    trace!("read event for {:?}", uid);
+                    loop {
+                        match self.connections[client].read_msg() {
+                            Ok(ReadResult::Msg(msg)) => {
+                                let mut h = self.handler.take().unwrap();
+                                h.message(self, &uid, msg);
+                                self.handler = Some(h);
+                            }
+                            Ok(ReadResult::None) => {
+                                self.connections_reregister.as_mut().unwrap().insert(client);
+                                break;
+                            }
+                            Ok(ReadResult::Closed) => {
+                                self.connections_closed.as_mut().unwrap().insert(client);
+                                break;
+                            }
+                            Err(e) => {
+                                error!("error reading from {:?}: {:?}", uid, e);
+                                self.connections_closed.as_mut().unwrap().insert(client);
+                                break;
+                            }
+                        }
+                    }
+                }
+                // writable
+                if events.is_writable(){
+                    trace!("write event for {:?}", uid);
+                    if let Err(e) = self.connections[client].write() {
+                        error!("write error for {:?}: {:?}", uid, e);
+                        self.connections_closed.as_mut().unwrap().insert(client);
+                    } else {
+                        self.connections_reregister.as_mut().unwrap().insert(client);
+                    }
+                }
+                // hup
+                if events.is_hup() {
+                    trace!("hup event for {:?}", uid);
+                    self.connections_closed.as_mut().unwrap().insert(client);
+                }
             }
         }
     }
 
     #[allow(unused_variables)]
     fn notify(&mut self, evloop: &mut EventLoop<Self>, msg: Self::Message) {
-        debug!("notify");
+        trace!("notify");
     }
 
     #[allow(unused_variables)]
     fn timeout(&mut self, evloop: &mut EventLoop<Self>, timeout: Self::Timeout) {
-        debug!("timeout");
+        trace!("timeout");
     }
 
     #[allow(unused_variables)]
     fn interrupted(&mut self, evloop: &mut EventLoop<Self>) {
-        debug!("interrupted");
+        trace!("interrupted");
     }
 
     fn tick(&mut self, evloop: &mut EventLoop<Self>) {
         self.register_new_connections(evloop);
+        self.reregister_connections(evloop);
         self.remove_closed_connections(evloop);
     }
 }
 
+trait ServerControl {
+    /// Send a msg to the destination. Will ignore a non existing connection
+    fn send(&mut self, uid: &ConnectionUid, msg: &[u8]);
+    /// Send a msg to the destination. Will ignore non existing connections
+    fn multicast(&mut self, uids: &mut Iterator<Item=&ConnectionUid>, msg: &[u8]);
+    fn close_connection(&mut self, uid: ConnectionUid);
+    fn shutdown(&mut self);
+}
+
+
+impl<H: ServerHandler> ServerControl for Server<H> {
+    fn send(&mut self, uid: &ConnectionUid, msg: &[u8]) {
+        if let Some(c) = self.connections.get_mut(uid.token) {
+            if &c.uid == uid {
+                c.send_msg(msg);
+                self.connections_reregister.as_mut().unwrap().insert(uid.token);
+            }
+        }
+    }
+    fn multicast(&mut self, uids: &mut Iterator<Item=&ConnectionUid>, msg: &[u8]) {
+        for uid in uids {
+            self.send(uid, msg);
+        }
+    }
+    fn close_connection(&mut self, uid: ConnectionUid) {
+        self.connections_closed.as_mut().unwrap().insert(uid.token);
+    }
+    fn shutdown(&mut self) {
+        self.shutdown = true;
+    }
+}
+
+enum ConnectionState {
+    ReadSize,
+    ReadData(usize),
+}
+
 struct Connection {
+    uid: ConnectionUid,
+    state: ConnectionState,
+    buf: RingBuf,
+    to_send: VecDeque<ByteBuf>,
     token: Token,
-    addr: SocketAddr,
     socket: TcpStream,
     interest: EventSet,
+}
+
+enum ReadResult {
+    // read a message
+    Msg(Vec<u8>),
+    // would block
+    None,
+    // connection closed
+    Closed,
+}
+
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct ConnectionUid {
+    id: u32,
+    token: Token,
+    addr: SocketAddr,
 }
 
 impl Connection {
     fn new(token: Token, socket: TcpStream) -> Self {
         Connection {
+            uid: ConnectionUid {
+                id: rand::random::<u32>(),
+                token: token,
+                addr: socket.peer_addr().unwrap()
+            },
+            state: ConnectionState::ReadSize,
+            buf: RingBuf::new(MAX_MSG_SIZE+MSG_HDR_SIZE),
+            to_send: VecDeque::new(),
             token: token,
-            addr: socket.peer_addr().unwrap(),
             socket: socket,
-            interest: EventSet::readable(),
+            interest: EventSet::readable() | EventSet::hup(),
         }
     }
 
     fn register<H: ServerHandler>(&mut self, evloop: &mut EventLoop<Server<H>>) -> Result<(), Error> {
         try!(evloop.register(&self.socket, self.token, self.interest, PollOpt::edge() | PollOpt::oneshot()));
-        debug!("registering new connection {}", self.addr);
         Ok(())
     }
 
     fn reregister<H: ServerHandler>(&mut self, evloop: &mut EventLoop<Server<H>>) -> Result<(), Error> {
-        debug!("reregistering connection {} for {:?}", self.addr, self.interest);
         try!(evloop.reregister(&self.socket, self.token, self.interest, PollOpt::edge() | PollOpt::oneshot()));
         Ok(())
     }
 
     fn deregister<H: ServerHandler>(&mut self, evloop: &mut EventLoop<Server<H>>) -> Result<(), Error> {
-        debug!("deregistering connection {}", self.addr);
         try!(evloop.deregister(&self.socket));
         Ok(())
     }
 
-    fn ready<H: ServerHandler>(&mut self, evloop: &mut EventLoop<Server<H>>, events: EventSet) {
-        debug!("events for {}: {:?}", self.addr, events);
+    // try to parse a message from the connection buffer
+    fn try_parse_msg(&mut self) -> Option<Vec<u8>> {
+        loop {
+            match self.state {
+                ConnectionState::ReadSize => {
+                    debug!("read header buf.remaining {}", self.buf.remaining());
+                    if self.buf.remaining() >= 4 {
+                        let size = network_to_u32(self.buf.bytes()) as usize;
+                        self.buf.advance(4);
+                        self.state = ConnectionState::ReadData(size);
+                        debug!("read header: {}", size);
+                    } else {
+                        return None;
+                    }
+                }
+                ConnectionState::ReadData(size) => {
+                    debug!("read msg buf.remaining {}", self.buf.remaining());
+                    if self.buf.remaining() >= size {
+                        self.state = ConnectionState::ReadSize;
+                        let mut msg = Vec::with_capacity(size);
+                        let r = self.buf.try_read_buf(&mut msg).unwrap().unwrap();
+                        debug_assert_eq!(size, r);
+                        return Some(msg);
+                    } else {
+                        return None;
+                    }
+                }
+            }
+        }
+    }
+
+    // try to read a whole message
+    fn read_msg(&mut self) -> Result<ReadResult, Error> {
+        if let Some(msg) = self.try_parse_msg() {
+            return Ok(ReadResult::Msg(msg));
+        }
+        loop {
+            match self.socket.try_read_buf(&mut self.buf) {
+                Ok(None) => {
+                    debug!("read: would block");
+                    return Ok(ReadResult::None);
+                }
+                Ok(Some(r)) => {
+                    debug!("read: {} bytes", r);
+                    if r == 0 {
+                        return Ok(ReadResult::Closed);
+                    }
+                    if let Some(msg) = self.try_parse_msg() {
+                        return Ok(ReadResult::Msg(msg));
+                    }
+                }
+                Err(e) => {
+                    debug!("read: error {}", e);
+                    // self.read_buf = Some(read_buf);
+                    return Err(Error::from(e));
+                }
+            }
+        }
+    }
+
+    fn send_msg(&mut self, msg: &[u8]) {
+        let mut buf = ByteBuf::mut_with_capacity(MSG_HDR_SIZE + msg.len());
+        let hdr: &[u8];
+        let len = (msg.len() as u32).to_be();
+        unsafe {
+            hdr = slice::from_raw_parts((&len as *const u32) as *const u8, 4);
+        }
+        buf.write_slice(hdr);
+        buf.write_slice(msg);
+        self.to_send.push_back(buf.flip());
+        self.interest.insert(EventSet::writable());
+    }
+
+    fn write(&mut self) -> Result<(), Error> {
+        while !self.to_send.is_empty() {
+            match self.socket.try_write_buf(&mut self.to_send[0]) {
+                Ok(Some(n)) => debug!("write to {:?}: {} bytes", self.uid, n),
+                Ok(None) => break, // retry later
+                Err(e) => return Err(Error::from(e)),
+            }
+            if self.to_send[0].remaining() == 0 {
+                self.to_send.pop_front().unwrap();
+            }
+        }
+        if self.to_send.is_empty() {
+            self.interest.remove(EventSet::writable());
+        }
+        Ok(())
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-struct ConnectionUid(u32, Token, SocketAddr);
-
-trait ServerControl {
-    fn send(&mut self, uid: &ConnectionUid, msg: ByteBuf);
-    fn multicast(&mut self, uids: Iterator<Item=&ConnectionUid>, msg: ByteBuf);
-    fn close_connection(&mut self, uid: &ConnectionUid);
-    fn shutdown(&mut self);
-}
-
+#[allow(unused_variables)]
 trait ServerHandler {
-    fn connection(&mut self, uid: ConnectionUid);
-    fn connection_closed(&mut self, uid: ConnectionUid);
-    fn message(&mut self, uid: ConnectionUid, msg: ByteBuf);
-    fn shutting_down(&mut self, err: Option<Error>);
+    fn connection(&mut self, server: &mut ServerControl, uid: ConnectionUid) {
+    }
+    fn connection_closed(&mut self, server: &mut ServerControl, uid: &ConnectionUid) {
+    }
+    fn message(&mut self, server: &mut ServerControl, uid: &ConnectionUid, msg: Vec<u8>);
+    fn shutting_down(&mut self, server: &mut ServerControl, err: Option<Error>) {
+    }
 }
 
 // ------------------------------------------------------
 
-impl ServerHandler for () {
-    fn connection(&mut self, uid: ConnectionUid) {}
-    fn connection_closed(&mut self, uid: ConnectionUid) {}
-    fn message(&mut self, uid: ConnectionUid, msg: ByteBuf) {}
-    fn shutting_down(&mut self, err: Option<Error>) {}
+struct MyHandler {
+    connections: HashSet<ConnectionUid>,
+}
+
+impl ServerHandler for MyHandler {
+    fn connection(&mut self, _server: &mut ServerControl, uid: ConnectionUid) {
+        self.connections.insert(uid);
+        debug!("handler new connection");
+    }
+    fn connection_closed(&mut self, _server: &mut ServerControl, uid: &ConnectionUid) {
+        self.connections.remove(uid);
+        debug!("handler disconnect");
+    }
+    fn message(&mut self, server: &mut ServerControl, _uid: &ConnectionUid, msg: Vec<u8>){
+        debug!("handler message called");
+        server.multicast(&mut self.connections.iter(), &msg[..]);
+    }
 }
 
 fn main() {
     env_logger::init().unwrap();
     let addr = "127.0.0.1:10000".parse().unwrap();
-    let mut server = Server::bind(&addr, ()).unwrap();
-    let mut evloop = EventLoop::new().unwrap();
-    server.register(&mut evloop).unwrap();
-    evloop.run(&mut server).unwrap();
+    let mut server = Server::bind(&addr, MyHandler{ connections: HashSet::new() }).unwrap();
+    server.run().unwrap();
 }
